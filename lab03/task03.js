1aaa8f4f2f123b470732fb9f773e70794cd819f1
Marina Dubovik <dubovik.m14@gmail.com>
2021-01-08 22:34:57 +0300

    update task#3, first class function

--- lab03/task03.js
+++ lab03/task03.js
@@ -4,3 +4,11 @@ const variable = 'Hello World';
 
 // clear function
 const add = (x, y) => x + y;
+
+
+// first-class function, function == Object
+// https://developer.mozilla.org/en-US/docs/Glossary/First-class_Function
+const helloWorld = function () {
+    console.log('Hello World');
+}
+helloWorld();
