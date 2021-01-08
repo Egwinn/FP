93b5a8fccfa3c9c9d50bdcdbcdf94dfc5b5c695b
Marina Dubovik <dubovik.m14@gmail.com>
2021-01-08 22:16:17 +0300

    add task#2, functional binary search

--- lab02/task02a.js
+++ lab02/task02a.js
@@ -13,3 +13,27 @@ function binarySearchImperative(data, target) {
 }
 
 console.log(binarySearchImperative([3, 4, 5, 6, 7, 8, 9], 7));
+
+// functional
+
+const binarySearchFunctional = (data, target, start, end) => {
+    if (end < 1) {
+        return data[0];
+    }
+    const middle = Math.floor((start + end) / 2);
+    if (target === data[middle]) {
+        return middle;
+    }
+    if (end - 1 === start) {        // left 2 final values
+        return Math.abs(data[start] - target) > Math.abs(data[end] - target) ? end : start;
+    }
+    if (target > data[middle]) {
+        return binarySearchFunctional(data, target, middle + 1, end);
+    }
+    if (target < data[middle]) {
+        return binarySearchFunctional(data, target, start, middle - 1);
+    }
+};
+
+const data = [3, 4, 5, 6, 7, 8, 9];
+console.log(binarySearchFunctional(data, 7, 0, data.length - 1));
