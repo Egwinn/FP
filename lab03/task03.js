87516bf0d86b851ce2e623065e37a1ca6a518b25
Marina Dubovik <dubovik.m14@gmail.com>
2021-01-08 22:40:42 +0300

    update task#3, higher order function

--- lab03/task03.js
+++ lab03/task03.js
@@ -12,3 +12,21 @@ const helloWorld = function () {
     console.log('Hello World');
 }
 helloWorld();
+
+
+// higher-order function is a function that can take another function as an argument, or that returns a function as a result.
+// https://www.sitepoint.com/higher-order-functions-javascript/
+function firstFunc() {
+    console.log('first');
+}
+function secondFunc() {
+    console.log('second');
+}
+function invoke(type, firstFunc, secondFunc) {
+    if (type === 'first') {
+        return firstFunc();
+    } else if (type === 'second') {
+        return secondFunc();
+    }
+}
+invoke('first', firstFunc, secondFunc);
