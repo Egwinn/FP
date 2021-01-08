fd66cdc6e7bb5bad62de7a9c51ce3ae111cf32b9
Marina Dubovik <dubovik.m14@gmail.com>
2021-01-08 22:21:48 +0300

    add task#2, imperative minmax algorithm

---
+++ lab02/task02b.js
@@ -0,0 +1,29 @@
+// 2.5. Напишите программу для нахождения минимального из чисел, являющихся максимальными в каждой 
+// из строк заданной прямоугольной матрицы.
+
+const matrix = [
+    [4, 45, 3],     // 45
+    [1, 1, 3],      // 3
+    [18, 19, 8]     // 19
+];
+
+// imperative
+
+function minMaxImperative(m) {
+    let min;
+    let max;
+    m.forEach(row => {
+        max = 0;
+        row.forEach(item => {
+            if (item > max || !max) {
+                max = item;
+            }
+        });
+        if (max < min || !min) {
+            min = max;
+        }
+    });
+    return min;
+}
+
+console.log(minMaxImperative(matrix));
