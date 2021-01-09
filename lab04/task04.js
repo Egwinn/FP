2824841c4d8ef8d88a1c5735d78474f4e83afb0b
Marina Dubovik <dubovik.m14@gmail.com>
2021-01-09 15:10:54 +0300

    update task#4. part#2

--- lab04/task04.js
+++ lab04/task04.js
@@ -7,3 +7,10 @@ console.log('--------------1--------------');
 console.log(modificate((x) => x * x, 4));
 console.log(modificate((x) => x * x * x, 3));
 console.log(modificate((x) => -x, 14));
+
+// 2
+function filter(collection, func) {
+    return collection.filter(item => func(item));
+}
+console.log('--------------2--------------');
+console.log(filter([1, 4, 3, 7, 15, 24], (x) => x < 10));
